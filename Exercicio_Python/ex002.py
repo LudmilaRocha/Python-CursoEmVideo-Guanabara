6ad53db4e089c541c74f7c034ffa7933d8d71704
Ludmila <ludmila030301@gmail.com>
2024-11-21 19:32:45 -0300

    Update ex002.py

---
+++ Exercicio_Python/ex002.py
@@ -0,0 +1,4 @@
+#Execute um  comando que questiona o nome do usuario e depois demonstra uma mensagem de Bem Vindo
+
+nome = input("Digite seu nome:")
+print("Bem Vindo {} a nossa plataforma!".format(nome))
