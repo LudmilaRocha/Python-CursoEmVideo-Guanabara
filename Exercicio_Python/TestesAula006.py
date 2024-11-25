2a3bf5dbb6aeb989c37015694caa8cc0fea98459
Ludmila <ludmila030301@gmail.com>
2024-11-25 19:57:53 -0300

    Update exercicio 006
    Tipos de variaveis e prints com estas variaveis

---
+++ Exercicio_Python/TestesAula006.py
@@ -0,0 +1,8 @@
+#verificando um tipo de variavel
+
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+soma = n1 + n2
+
+#cocatenação
+print(f"A soma entre {n1} e {n2} é: {soma}")
---
+++ Exercicio_Python/ex003.py
@@ -0,0 +1,12 @@
+#aplicação do exercicio da aula 06
+#soma
+#sempre necessário acrescentar o tipo da variavel
+#int(7,-4,0,987), float(4.75,8.1,-15.23,7.0),
+# bool(True e False / Sempre precisa ter letras maiusculas),
+# str (string)/"Ola", "7.0"
+
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um outro numero:"))
+Soma = n1 + n2
+print("A soma do valor é = ",Soma)
+#os prints mudam tambem, print("A soma vale()".format(s))
